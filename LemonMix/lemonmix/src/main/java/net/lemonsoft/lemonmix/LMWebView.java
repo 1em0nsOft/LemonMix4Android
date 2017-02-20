dd113b1b05e128c2d0c8a115adf9781b76e0aa25
LiuRi <liuri@lemonsoft.net>
2017-02-20 17:49:54 +0800

    更名webView为LMWebView

--- LemonMix/lemonmix/src/main/java/net/lemonsoft/lemonmix/LMWebView.java
+++ LemonMix/lemonmix/src/main/java/net/lemonsoft/lemonmix/LMWebView.java
@@ -12,29 +12,29 @@ import java.util.Map;
  * Created by LiuRi on 2017/2/20.
  */
 
-public class WebView extends com.tencent.smtt.sdk.WebView {
+public class LMWebView extends com.tencent.smtt.sdk.WebView {
 
-    public WebView(Context context) {
+    public LMWebView(Context context) {
         super(context);
         this._init();
     }
 
-    public WebView(Context context, AttributeSet attributeSet) {
+    public LMWebView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         this._init();
     }
 
-    public WebView(Context context, AttributeSet attributeSet, int i) {
+    public LMWebView(Context context, AttributeSet attributeSet, int i) {
         super(context, attributeSet, i);
         this._init();
     }
 
-    public WebView(Context context, AttributeSet attributeSet, int i, boolean b) {
+    public LMWebView(Context context, AttributeSet attributeSet, int i, boolean b) {
         super(context, attributeSet, i, b);
         this._init();
     }
 
-    public WebView(Context context, AttributeSet attributeSet, int i, Map<String, Object> map, boolean b) {
+    public LMWebView(Context context, AttributeSet attributeSet, int i, Map<String, Object> map, boolean b) {
         super(context, attributeSet, i, map, b);
         this._init();
     }
--- LemonMix/lemonmix_samples/src/main/java/net/lemonsoft/lemonmix_samples/MainActivity.java
+++ LemonMix/lemonmix_samples/src/main/java/net/lemonsoft/lemonmix_samples/MainActivity.java
@@ -6,18 +6,18 @@ import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-import net.lemonsoft.lemonmix.WebView;
+import net.lemonsoft.lemonmix.LMWebView;
 
 public class MainActivity extends Activity {
 
-    private WebView webView;
+    private LMWebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         RelativeLayout root = new RelativeLayout(this);
         this.setContentView(root);
-        webView = new WebView(this);
+        webView = new LMWebView(this);
         root.addView(webView, new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         webView.setBackgroundColor(Color.RED);
         webView.loadUrl("http://www.baidu.com");
